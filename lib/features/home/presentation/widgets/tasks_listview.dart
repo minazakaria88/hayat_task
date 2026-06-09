62e77e6a05974a4cc4d4232f1da5e0d94a9b1c00
w.i <mynaz7142@gmail.com>
2026-06-09 11:04:13 +0300

    show error snackbar when delete task fail

--- lib/features/home/presentation/widgets/tasks_listview.dart
+++ lib/features/home/presentation/widgets/tasks_listview.dart
@@ -14,43 +14,54 @@ class TasksListview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final homeCubit = context.read<HomeCubit>();
-    return ListView.builder(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      itemCount: tasks.length,
-      itemBuilder: (context, index) {
-        final task = tasks[index];
-        return BlocSelector<HomeCubit, HomeState, bool>(
-          selector: (state) {
-            return state.deletedTaskId == task.id &&
-                state.deleteTaskStatus == DeleteTaskStatus.loading;
-          },
-          builder: (context, state) => Stack(
-            children: [
-              TaskDetailsCard(
-                model: task,
-                onEdit: () {
-                  context.pushNamed(
-                    AppRouter.createEditTask.name,
-                    extra: {'cubit': homeCubit, 'task': task},
-                  );
-                },
-                onDelete: () async {
-                  homeCubit.deleteTodo(id: task.id);
-                },
-              ),
-              if (state)
-                Positioned.fill(
-                  child: Container(
-                    color: Colors.grey.withValues(alpha: 0.5),
-                    width: double.infinity,
-                    height: double.infinity,
-                    child: const Center(child: CircularProgressIndicator()),
-                  ),
-                ),
-            ],
-          ),
-        );
+    return BlocListener<HomeCubit, HomeState>(
+      listenWhen: (previous, current) =>
+          previous.deleteTaskStatus != current.deleteTaskStatus,
+      listener: (context, state) {
+        if (state.deleteTaskStatus == DeleteTaskStatus.failure) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.errorMessage ?? '')));
+        }
       },
+      child: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          final task = tasks[index];
+          return BlocSelector<HomeCubit, HomeState, bool>(
+            selector: (state) {
+              return state.deletedTaskId == task.id &&
+                  state.deleteTaskStatus == DeleteTaskStatus.loading;
+            },
+            builder: (context, state) => Stack(
+              children: [
+                TaskDetailsCard(
+                  model: task,
+                  onEdit: () {
+                    context.pushNamed(
+                      AppRouter.createEditTask.name,
+                      extra: {'cubit': homeCubit, 'task': task},
+                    );
+                  },
+                  onDelete: () async {
+                    homeCubit.deleteTodo(id: task.id);
+                  },
+                ),
+                if (state)
+                  Positioned.fill(
+                    child: Container(
+                      color: Colors.grey.withValues(alpha: 0.5),
+                      width: double.infinity,
+                      height: double.infinity,
+                      child: const Center(child: CircularProgressIndicator()),
+                    ),
+                  ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
